68947696c9fcc2e76192c0747599ac4dcbbc05d0
mrityunjay raj <mrityunjayyadav59@gmail.com>
2023-10-29 05:19:07 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,56 @@
+function GetInfo() {
+    var newName = document.getElementById("cityInput");
+    var cityName = document.getElementById("cityName");
+    cityName.innerHTML = "--" + newName.value + "--";
+
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + newName.value + '&appid=04230a81bd13f988cb1042703e01a231')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Getting the min and max values for each day
+            for (let i = 0; i < 5; i++) {
+                document.getElementById("day" + (i + 1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min - 273.15).toFixed(1) + "°";
+            }
+
+            for (let i = 0; i < 5; i++) {
+                document.getElementById("day" + (i + 1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max - 273.15).toFixed(2) + "°";
+            }
+
+            // Getting Weather Icons
+            for (let i = 0; i < 5; i++) {
+                document.getElementById("img" + (i + 1)).src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
+            }
+
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            alert('Something went wrong. Please check the console for more details.');
+        });
+}
+
+function DefaultScreen() {
+    document.getElementById("cityInput").defaultValue = "London";
+    GetInfo();
+}
+
+// Getting and displaying the text for the upcoming five days of the week
+var d = new Date();
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+function CheckDay(day) {
+    if (day + d.getDay() > 6) {
+        return day + d.getDay() - 7;
+    } else {
+        return day + d.getDay();
+    }
+}
+
+for (let i = 0; i < 5; i++) {
+    document.getElementById("day" + (i + 1)).innerHTML = weekday[CheckDay(i)];
+}
+
